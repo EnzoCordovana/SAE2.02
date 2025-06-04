9b2771e648b390b33d53c6cc80b76a157b4888e4
Oliver_Gudenschwager <oliver.gudenschwager@etu.univ-amu.fr>
2025-06-04 11:13:06 +0200

    Modification Exo 2

---
+++ Exercice2.py
@@ -0,0 +1,107 @@
+import numpy as np
+import math
+
+def matrice_transposee(A):
+    A = np.array(A)
+    if A.ndim == 1:
+        # Calcule la dimension de la matrice
+        d = int(np.sqrt(len(A)))
+        # Redimensionner la matrice en une matrice carrée
+        # [a,b,c,d] => [[a,b],[c,d]]
+        A = A.reshape((d, d))
+    # Retourne directement la transposée
+    return A.T
+
+def matrice_stochastique(A):
+    # Transforme la matrice en matrice numpy en float
+    A = np.array(A, dtype=float)
+    # Somme par colonne
+    som_col = A.sum(axis=0)
+    # Remplace 0 par 1 pour éviter division par zéro
+    som_col[som_col == 0] = 1
+    # Normalise chaque colonne
+    # La somme de la colonne fait 1
+    Q = A / som_col
+    return Q
+
+web = [
+    0, 0, 0, 0, 0,
+    1, 0, 0, 0, 0,
+    1, 0, 0, 1, 0,
+    1, 1, 0, 0, 0,
+    1, 1, 0, 0, 0
+]
+
+alpha = 0.85
+
+def matrice_transition_P(A, alpha):
+    for i in range(len(A)):
+        colSom = np.sum(A[:, i]) # Stockage de la somme d'une colonne
+        if colSom == 0:
+            A[:, i] = [1/len(A)] # Si somme = 0, alors la colonne entière devienne 1/N
+        else:
+            A[:, i] = alpha * A[:, i] + (1-alpha)/len(A) # Sinon, chaque cellule devienne alpha * cellule + (1-alpha)/N
+    return A
+
+def puissance_iteree_v2(A, p, alpha):
+    """
+    Fonction puissance_iteree qui prend en entrée 
+    une matrice A, une précision p et qui retourne 
+    le vecteur propre.
+    
+    A       matrice, doit être un tableau
+    p       précision, doit être un entier   
+    """
+
+    A = np.array(A)
+
+    # Transpose et stochastique la matrice
+    Q = matrice_stochastique(matrice_transposee(A))
+
+    # Traitement par colonne
+    matrice_transition_P(Q, alpha)
+
+    d = Q.shape[0]
+    r = np.ones(d) / d # Normalisation
+
+    while True:
+        ancien_r = r
+        r = np.dot(Q, r)
+        # On compare deux matrices selon la précision p
+        if np.allclose(r, ancien_r, atol=p):
+            return ancien_r
+
+r = puissance_iteree_v2(web, 1e-6, alpha)
+
+# On crée une liste de tuples (index_page, score)
+page_rank = list(enumerate(r, start=1))
+
+print("PageRank :")
+for page, rank in page_rank:
+    print(f"Page {page}\t: {rank:.4f}")
+print("\r")
+print("r = Pr approximativement ?")
+A = np.array(web)
+Q = matrice_stochastique(matrice_transposee(A))
+Pr = r.dot(matrice_transition_P(Q, alpha))
+res= []
+tabRes = abs(np.subtract(Pr, r))
+for i in tabRes:
+    if i < 0.85:
+        res.append(True)
+    else:
+        res.append(False)
+print(res)
+
+"""
+1) Si on applique l'algorithme de la partie 1 au graphe de cette partie,
+celui-ci plantera parce que dans ce graphe on a un puit, ce qui produit une colonne
+nulle dans la matrice. Le problème est que vu qu'on veux multiplier par un vecteur
+aléatoire, cette colonne fera en sort que le rank pour ce sommet soit zero, ce qui ne
+fait pas de sens car il sera forcémént pointé par quelqu'un, et il aura donc forcément
+un score non nul.
+
+2) En appliquant la nouvelle matrice de transition, on attribue donc le facteur
+d'amortissement, ce qui nous permet d'attribuer aux cellules nulles une valeur
+non nulle pour que le sommet correspondant aie un score même s'il s'agit d'un puit.
+"""
