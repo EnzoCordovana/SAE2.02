4d32a54c0458e958e1a015f4bd4988d892c15a11
Namdas <amdasise48@gmail.com>
2025-06-04 16:58:41 +0200

    Ajout Diagrammes en Bandes avec Partie1 et 3.2

--- Partie3NewCode.py
+++ Partie3NewCode.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 def matrice_transposee(A):
     A = np.array(A)
@@ -92,6 +93,29 @@ webWithHubAndAuthority = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0,   #Page 14 [Hub] -> {10, 11, 13}
 ]
 
+webWithHubAndAuthorityV2 = [
+    0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+    1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0,
+    1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,   #Page 15 [Hub] -> {1, 5, 7, 8, 9, 10, 11}
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   #Page 16 [Authority] -> {6, 10, 14}
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0,   #Page 17 [Authority] -> {2, 3, 4, 5, 11, 12, 13, 14}
+    1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   #Page 18 [Hub] -> {1, 2, 6,}
+]
+
+
+
 # Partie 3.3 - Augementation du score des pages X
 # Matrice de boost pour les pages X en ajoutant des liens entrants vers une page ciblée
 webPageXScoreBoost = [
@@ -161,25 +185,32 @@ r = puissance_iteree_v2(web, 1e-6, alpha)
 rH = puissance_iteree_v2(webWithHub, 1e-6, 0.85)
 rA = puissance_iteree_v2(webWithAuthority, 1e-6, 0.85)
 rHA = puissance_iteree_v2(webWithHubAndAuthority, 1e-6, 0.85)
+rHA2 = puissance_iteree_v2(webWithHubAndAuthorityV2, 1e-6, 0.85)
 
 # On crée une liste de tuples (index_page, score)
 page_rank = list(enumerate(r, start=1))
 page_rank2 = list(enumerate(rH, start=1))
 page_rank3 = list(enumerate(rA, start=1))
 page_rank4 = list(enumerate(rHA, start=1))
+page_rank4_v2 = list(enumerate(rHA2, start=1))
 
 rB = puissance_iteree_v2(webPageXScoreBoost, 1e-6, 0.85)
 page_rank5 = list(enumerate(rB, start=1))
 
-print("PageRank :")
+
 # Liste des précisions pour les tests
 precisions = [1e-2, 1e-4, 1e-5, 1e-6, 1e-8, 1e-10]
 
 # Liste des alphas pour les tests
 alphas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
+print("PageRank :")
 for page, rank in page_rank:
     print(f"Page {page}\t: {rank:.4f}")
+
+rank1 = []
+for i in page_rank:
+    rank1.append(i[1])
 
 print("\n")
 print("===========================")
@@ -199,9 +230,22 @@ for page, rank in page_rank3:
 
 print("\n")
 print("===========================")
-print("\nPageRank avec un Hub et une Authority :")
+print("\nPageRank avec un Hub et une Authority (Version1) :")
 
 for page, rank in page_rank4:
+    print(f"Page {page}\t: {rank:.4f}")
+
+rank2 = []
+for i in page_rank4:
+    rank2.append(i[1])
+
+print(rank2)
+
+print("\n")
+print("===========================")
+print("\nPageRank avec un Hub et une Authority (Version2):")
+
+for page, rank in page_rank4_v2:
     print(f"Page {page}\t: {rank:.4f}")
 
 print("\n")
@@ -215,9 +259,32 @@ for p in precisions:
     print(f"\nCalcul avec précision {p}:")
     r = puissance_iteree_v2(web, p, 0.85)
 
+   
+
 # On effectue les tests avec différents alphas
 for a in alphas:
     print(f"\nCalcul avec alpha {a}:")
     r = puissance_iteree_v2(web, 1e-6, a)
 
 
+
+def creerListePages(n):
+    listePages = []
+    for i in range(n):
+        listePages.append("Page " + str(i+1))
+    return listePages
+
+qtePages = 14
+
+listePages = creerListePages(qtePages)
+
+matriceSansHubEtAut = rank1
+matriceAvecHubEtAut = rank2
+
+plt.bar(listePages, matriceSansHubEtAut, align='center', label="Ranks sans Hubs et Autorités")
+plt.bar(listePages, matriceAvecHubEtAut, color='red', width=0.5, label="Ranks avec Hubs et Autorités")
+
+plt.title("Impact des scores avec Hubs et Autorités")
+plt.ylabel("Rank [0-1]")
+plt.legend()
+plt.show()
